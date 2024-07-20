6282bd8911ec1daad5a4bf646d91e64f52375421
abcd-EGH <wlghks7790@gmail.com>
2024-07-20 16:35:29 +0900

    240720 modify a way to access KOPIS OPENAPI KEY: from config.json to environment variable

--- json_test.py
+++ json_test.py
@@ -1,8 +1,11 @@
-import json
+import os
 
-# 설정 파일에서 인증키를 불러옵니다.
-with open("config.json", "r") as config_file:
-    config = json.load(config_file)
-    kopis_api_key = config["KOPIS_OPENAPI_KEY"]
+# # 설정 파일에서 인증키를 불러옴
+# with open("config.json", "r") as config_file:
+#     config = json.load(config_file)
+#     kopis_api_key = config["KOPIS_OPENAPI_KEY"]
+
+# 환경 변수로 인증키를 불러옴
+kopis_api_key = os.getenv("KOPIS_OPENAPI_KEY")
 
 print(kopis_api_key)
