1ca54eb1600d785b2ccd2883fa4846602c43d4d0
Jihwan Lee <wlghks7790@gmail.com>
2024-09-01 04:38:25 +0900

    [FIX] path of FAISS

--- app/FAISS.py
+++ app/FAISS.py
@@ -2,7 +2,7 @@ import faiss
 import numpy as np
 import mysql.connector
 import os
-from app.CLIP import CLIP
+from CLIP import CLIP
 import torch
 
 # FAISS 인덱스를 불러오는 함수
--- app/app.py
+++ app/app.py
@@ -148,4 +148,4 @@ def bad_request(error):
     return jsonify({'error': 'Bad Request'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
